00796beb144deaa1f70611d07e1f67cb7db75510
Red Chien <redchien@viridiasolutions.com>
2026-02-20 16:17:24 +0800

    feat: Implement atomic button component with customizable properties and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AtomicLink } from './shared/components/atomic-link/atomic-link';
+import { AtomicButton } from './shared/components/atomic-button/atomic-button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AtomicLink],
+  imports: [RouterOutlet, AtomicLink, AtomicButton, MatDividerModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/components/atomic-button/atomic-button.ts
+++ src/app/shared/components/atomic-button/atomic-button.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-atomic-button',
   imports: [],
   templateUrl: './atomic-button.html',
   styleUrl: './atomic-button.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AtomicButton {}
+export class AtomicButton {
+  disabled = input<boolean>(false);
+  type = input<'button' | 'submit' | 'reset'>('button');
+
+  variant = input<'contained' | 'outlined' | 'text'>('contained');
+  color = input<'primary' | 'success' | 'warn' | 'danger' | 'secondary'>('primary');
+  size = input<'normal' | 'small'>('normal');
+  shape = input<'rectangle' | 'circle' | 'square'>('rectangle');
+
+  BASIC_CLASS = 'atomic-button';
+  rootClass = computed(() => {
+    return [
+      `${this.BASIC_CLASS}--${this.variant()}`,
+      `${this.BASIC_CLASS}--${this.color()}`,
+      `${this.BASIC_CLASS}--${this.shape()}`,
+      `${this.BASIC_CLASS}--${this.size()}`,
+    ];
+  });
+}
