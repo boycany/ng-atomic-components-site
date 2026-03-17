6d1541140c469e4221d15178b3eb9a8737988953
Red Chien <redchien@viridiasolutions.com>
2026-03-17 15:38:15 +0800

    feat: Add the breadcrumb with icon and accessibility

--- src/app/pages/breadcrumb-showcase/breadcrumb-showcase.ts
+++ src/app/pages/breadcrumb-showcase/breadcrumb-showcase.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  TemplateRef,
+  viewChild,
+} from '@angular/core';
 import { AtomicBreadcrumb } from '../../shared/components/atomic-breadcrumb/atomic-breadcrumb';
 import { BreadcrumbItem } from '../../shared/components/atomic-breadcrumb/atomic-breadcrumb.types';
+import { MatIconModule } from '@angular/material/icon';
 
 interface SeparatorOption {
   label: string;
@@ -9,7 +16,7 @@ interface SeparatorOption {
 
 @Component({
   selector: 'app-breadcrumb-showcase',
-  imports: [AtomicBreadcrumb],
+  imports: [AtomicBreadcrumb, MatIconModule],
   templateUrl: './breadcrumb-showcase.html',
   styleUrl: './breadcrumb-showcase.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -34,14 +41,56 @@ export class BreadcrumbShowcase {
 
   protected readonly twoItems: BreadcrumbItem[] = [{ label: 'Home', to: '/' }, { label: 'About' }];
 
-  protected readonly deepItems: BreadcrumbItem[] = [
-    { label: 'Home', to: '/' },
-    { label: 'Products', to: '/products' },
-    { label: 'Electronics', to: '/products/electronics' },
-    { label: 'Computers', to: '/products/electronics/computers' },
-    { label: 'Laptops', to: '/products/electronics/computers/laptops' },
-    { label: 'MacBook Pro' },
-  ];
+  homeIconTmpl = viewChild<TemplateRef<unknown>>('homeIconTmpl');
+  productIconTmpl = viewChild<TemplateRef<unknown>>('productIconTmpl');
+  electronicIconTmpl = viewChild<TemplateRef<unknown>>('electronicIconTmpl');
+  computerIconTmpl = viewChild<TemplateRef<unknown>>('computerIconTmpl');
+  laptopIconTmpl = viewChild<TemplateRef<unknown>>('laptopIconTmpl');
+  laptopMacIconTmpl = viewChild<TemplateRef<unknown>>('laptopMacIconTmpl');
+  protected readonly labelWithIconItems = computed<BreadcrumbItem[]>(() => {
+    return [
+      { label: 'Home', to: '/', iconTmpl: this.homeIconTmpl() },
+      { label: 'Products', to: '/products', iconTmpl: this.productIconTmpl() },
+      { label: 'Electronics', to: '/products/electronics', iconTmpl: this.electronicIconTmpl() },
+      {
+        label: 'Computers',
+        to: '/products/electronics/computers',
+        iconTmpl: this.computerIconTmpl(),
+      },
+      {
+        label: 'Laptops',
+        to: '/products/electronics/computers/laptops',
+        iconTmpl: this.laptopIconTmpl(),
+      },
+      { label: 'MacBook Pro', iconTmpl: this.laptopMacIconTmpl() },
+    ];
+  });
+
+  protected readonly iconItems = computed<BreadcrumbItem[]>(() => {
+    return [
+      { label: 'Home', to: '/', iconTmpl: this.homeIconTmpl(), iconOnly: true },
+      { label: 'Products', to: '/products', iconTmpl: this.productIconTmpl(), iconOnly: true },
+      {
+        label: 'Electronics',
+        to: '/products/electronics',
+        iconTmpl: this.electronicIconTmpl(),
+        iconOnly: true,
+      },
+      {
+        label: 'Computers',
+        to: '/products/electronics/computers',
+        iconTmpl: this.computerIconTmpl(),
+        iconOnly: true,
+      },
+      {
+        label: 'Laptops',
+        to: '/products/electronics/computers/laptops',
+        iconTmpl: this.laptopIconTmpl(),
+        iconOnly: true,
+      },
+      { label: 'MacBook Pro', iconTmpl: this.laptopMacIconTmpl(), iconOnly: true },
+    ];
+  });
 
   protected readonly staticItems: BreadcrumbItem[] = [
     { label: 'Home' },
--- src/app/shared/components/atomic-breadcrumb/atomic-breadcrumb.types.ts
+++ src/app/shared/components/atomic-breadcrumb/atomic-breadcrumb.types.ts
@@ -1,6 +1,9 @@
+import { TemplateRef } from '@angular/core';
 import { LinkTo } from '../atomic-link/atomic-link.utils';
 
 export interface BreadcrumbItem {
   to?: LinkTo;
   label: string;
+  iconTmpl?: TemplateRef<unknown>;
+  iconOnly?: boolean;
 }
