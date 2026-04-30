a3ef535d9c9eed44709ffd3b1a4c2f32766cb82d
Red Chien <redchien@viridiasolutions.com>
2026-04-30 16:15:39 +0800

    test: track mouse feature

--- src/app/shared/directives/track-mouse/track-mouse.ts
+++ src/app/shared/directives/track-mouse/track-mouse.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, inject, signal } from '@angular/core';
+import { clamp } from '../../helpers/clamp.helper';
 
 @Directive({
   selector: '[appTrackMouse]',
@@ -14,25 +15,12 @@ export class TrackMouseDirective {
   readonly y = signal<number>(0);
   readonly el = inject(ElementRef).nativeElement;
 
-  // constructor() {
-  //   console.log('TrackMouseDirective initialized');
-  // }
-
   onMouseMove(event: MouseEvent) {
-    // console.log('this.el :>> ', this.el);
     const rect = this.el.getBoundingClientRect();
-    // console.log('rect :>> ', rect);
-    // console.log('event :>> ', event);
-    const x = this.clamp(event.clientX - rect.left, 0, rect.width);
-    const y = this.clamp(event.clientY - rect.top, 0, rect.height);
+    const x = clamp(0, event.clientX - rect.left, rect.width);
+    const y = clamp(0, event.clientY - rect.top, rect.height);
 
     this.x.set(x);
     this.y.set(y);
-
-    // console.log(this.x(), this.y());
-  }
-
-  clamp(min: number, num: number, max: number) {
-    return Math.min(Math.max(num, min), max);
   }
 }
