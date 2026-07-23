04e059ca43757b31e691b7a410deecc49f6352d3
yh.chien <yueh.hung.chien@gmail.com>
2026-07-23 15:18:46 +0800

    feat: add ItemContainerDirective for custom item rendering and update structural directive usage in ItemSelector

--- src/app/shared/components/item-selector/item-template.directive.ts
+++ src/app/shared/components/item-selector/item-template.directive.ts
@@ -1,8 +1,20 @@
 import { Directive, inject, TemplateRef } from '@angular/core';
 
+export interface ItemTemplateContext {
+  readonly $implicit: string;
+}
+
 @Directive({
   selector: '[appItemTemplate]',
 })
 export class ItemTemplateDirective {
-  readonly template = inject(TemplateRef);
+  readonly template = inject(TemplateRef<ItemTemplateContext>);
+
+  // It helps compiler to know the context type and avoid unknown, it doesn't execute in runtime
+  static ngTemplateContextGuard(
+    _: ItemTemplateDirective,
+    ctx: unknown
+  ): ctx is ItemTemplateContext {
+    return true;
+  }
 }
